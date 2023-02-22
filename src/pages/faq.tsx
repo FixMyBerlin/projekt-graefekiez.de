7857db49e839e3fc4fbe1db2913c9ddf826ec0f6
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-02-22 15:56:06 +0100

    migrate and configure faq components

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,21 +1,210 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable jsx-a11y/no-redundant-roles */
 import { HeadFC, PageProps } from 'gatsby'
 import React from 'react'
 import { Layout, MetaTags } from '~/components/layouts'
-import { Container } from '~/components/layouts/Container'
 import { Hero } from '~/components/layouts/Hero/Hero'
-import { PageHomeFaq } from '~/components/PageHome/PageHomeFaq'
+import { FaqItem } from '~/components/PageFaq/FaqItem'
+import { H1, H3, Prose } from '~/components/typography'
 
-const ForschungPage: React.FC<PageProps> = () => {
+export type TQuestion = {
+  id: number
+  question: string
+  answer: string
+}
+
+export type TFaqs = {
+  carFaqs: TQuestion[][]
+  pedestrianFaqs: TQuestion[][]
+}
+
+export const faqs: TFaqs = {
+  carFaqs: [
+    [
+      {
+        id: 1,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 2,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 3,
+        question: 'Warum wählen wir diesen Ansatz?',
+        answer:
+          'Wir müssen dringend die Verkehrssicherheit im Bezirk erhöhen. Das dies notwendig ist, zeigen immer wieder schwere oder gar tödliche Unfälle mit Rad- oder Fußbeteiligung. Wir sind den Verkehrsteilnehmenden verpflichtet, die besonders gefährdet sind. Dazu gehören z.B. Kinder, ältere Menschen – und im Allgemeinen alle, die zu Fuß oder mit dem Rad unterwegs sind. Wir wählen einen flächendeckenden Ansatz, um die Umweltgerechtigkeit im Bezirk zu erhöhen. Die Daten des Umweltatlas zeigen, dass die Wohngebiete in XHain unterschiedlich stark von negativen Umweltbelastungen betroffen sind. Mit zunehmenden Hitzesommern steigt der Bedarf für qualitativ hochwertige öffentliche Räume. Gerade in Gebieten des Bezirks mit hoher Einwohner*innen-Dichte und geringer Grünraumversorgung ist dies wichtig.',
+      },
+    ],
+    [
+      {
+        id: 4,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 5,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 6,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+    ],
+    [
+      {
+        id: 7,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 8,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 9,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+    ],
+  ],
+  pedestrianFaqs: [
+    [
+      {
+        id: 1,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 2,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 3,
+        question: 'Warum wählen wir diesen Ansatz?',
+        answer:
+          'Wir müssen dringend die Verkehrssicherheit im Bezirk erhöhen. Das dies notwendig ist, zeigen immer wieder schwere oder gar tödliche Unfälle mit Rad- oder Fußbeteiligung. Wir sind den Verkehrsteilnehmenden verpflichtet, die besonders gefährdet sind. Dazu gehören z.B. Kinder, ältere Menschen – und im Allgemeinen alle, die zu Fuß oder mit dem Rad unterwegs sind. Wir wählen einen flächendeckenden Ansatz, um die Umweltgerechtigkeit im Bezirk zu erhöhen. Die Daten des Umweltatlas zeigen, dass die Wohngebiete in XHain unterschiedlich stark von negativen Umweltbelastungen betroffen sind. Mit zunehmenden Hitzesommern steigt der Bedarf für qualitativ hochwertige öffentliche Räume. Gerade in Gebieten des Bezirks mit hoher Einwohner*innen-Dichte und geringer Grünraumversorgung ist dies wichtig.',
+      },
+    ],
+    [
+      {
+        id: 4,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 5,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 6,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+    ],
+    [
+      {
+        id: 7,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 8,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+      {
+        id: 9,
+        question: 'Fallen Parkplätze weg?',
+        answer:
+          'Ja es fallen parkplätze weg, allerding nur 0,000001%. mehr dazu sehen sie hier in der Statistik',
+      },
+    ],
+  ],
+}
+
+const FaqPage: React.FC<PageProps> = () => {
+  const { carFaqs, pedestrianFaqs } = faqs
   return (
     <Layout>
-      <Hero title="FAQ" subtitle="Foo" />
-      <Container colorClass="bg-white mx-auto max-w-prose">
-        <PageHomeFaq />
-      </Container>
+      <Hero title="Häufige Fragen" subtitle="und Antworten" />
+      <section className="mx-auto w-full max-w-7xl py-6 px-4 lg:mt-6 lg:py-8 lg:px-24">
+        <Prose>
+          <div className="mx-auto max-w-5xl px-4">
+            <H1>Antworten auf häufige Fragen</H1>
+
+            <H3 line>
+              <strong className="text-blue-600">Veränderungen für Autofahrende</strong>
+            </H3>
+
+            <ul
+              role="list"
+              className="mt-16 grid max-w-2xl grid-cols-1 gap-y-2 gap-x-8 lg:max-w-none lg:grid-cols-3"
+            >
+              {carFaqs.map((column, columnIndex) => (
+                <li key={columnIndex}>
+                  <ul role="list" className="flex flex-col gap-8">
+                    {column.map((faq) => (
+                      <li key={faq.id}>
+                        <FaqItem question={faq} />
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="mx-auto mt-16 max-w-5xl px-4">
+            <H3 line>
+              <strong className="text-blue-600">Fragen zum Fußverkehr</strong>
+            </H3>
+
+            <ul
+              role="list"
+              className="mt-16 grid max-w-2xl grid-cols-1 gap-y-2 gap-x-8 lg:max-w-none lg:grid-cols-3"
+            >
+              {pedestrianFaqs.map((column, columnIndex) => (
+                <li key={columnIndex}>
+                  <ul role="list" className="flex flex-col gap-8">
+                    {column.map((faq) => (
+                      <li key={faq.id}>
+                        <FaqItem question={faq} />
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Prose>
+      </section>
     </Layout>
   )
 }
 
-export default ForschungPage
+export default FaqPage
 
-export const Head: HeadFC = () => <MetaTags title="Forschung" />
+export const Head: HeadFC = () => <MetaTags title="FAQ" />
