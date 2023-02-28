ff2b36cb191a5c14a7f21e7ff2b645b9dbca4914
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-02-28 19:07:37 +0100

    update text

--- src/components/PageHome/PageHomeMapText.tsx
+++ src/components/PageHome/PageHomeMapText.tsx
@@ -4,12 +4,11 @@ import { H1, Prose } from '../typography'
 export const PageHomeMapText: React.FC = () => {
   return (
     <section className="mb-10">
-      <H1>Was passiert wo?</H1>
+      <H1>Was genau passiert 2023 im Kiez?</H1>
       <Prose>
         <p>
-          Hier sollte die Karte erläutert werden mit den verschiedenen Layern, detailierter können
-          die Einzelnen Themen dann unten erklärt werden. Insgesamt werden XY km2 Parkflächen
-          umgenutzt. Ein Teil dieser Flächen wird zu folgenden Zwecken genutzt: Lieferzonen:{' '}
+          Auf der interaktiven Karte sehen Sie alle Planungen für die kommenden Monate. Sie können
+          die einzelnen Themen ein- oder ausblenden.
         </p>
       </Prose>
     </section>
