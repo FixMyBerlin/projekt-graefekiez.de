f23d2ae454213d84b0a60dd99c6319f683eb792d
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-01 14:46:08 +0100

    update Container to avoid zero padding

--- src/components/layouts/Container.tsx
+++ src/components/layouts/Container.tsx
@@ -13,7 +13,7 @@ export const Container: React.FC<Props> = ({ colorClass, grid = false, children 
       <div
         className={clsx(
           { 'grid gap-10 md:grid-cols-3 ': grid },
-          'mx-auto max-w-7xl px-5 py-10 xl:px-0'
+          'mx-auto max-w-7xl px-5 py-10 2xl:px-0'
           // 'mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'
         )}
       >
