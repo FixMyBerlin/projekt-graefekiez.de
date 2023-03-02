bf4c99fd2fd3cdcda19a1a2dcc8ac02859334f10
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-02 11:22:31 +0100

    update Layout Footer

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, linkStyles } from '~/components/links'
 import TwitterIcon from '../../../images/twitterIcon.inline.svg'
+import { Container } from '../Container'
 
 const aboutItems = [
   {
@@ -21,43 +22,45 @@ const informationItems = [
 
 export const Footer: React.FC = () => {
   return (
-    <div className="flex-col bg-gray-800 py-10 px-4 text-gray-300">
-      <div className="grid grid-cols-2">
-        <div>
-          <p className="index uppercase text-gray-400">About</p>
-          <ul className="mt-4 flex flex-col gap-4">
-            {aboutItems.map((item) => (
-              <li key={item.title}>
-                <Link href={item.href}>{item.title}</Link>
-              </li>
-            ))}
-            <a
-              target="_blank"
-              className={linkStyles}
-              href="https://www.berlin.de/ba-friedrichshain-kreuzberg/aktuelles/pressemitteilungen/"
-              rel="noreferrer"
-            >
-              Presse
-            </a>
-          </ul>
+    <div className="mt-40 flex-col bg-gray-800 py-10 px-4 text-gray-300">
+      <Container>
+        <div className="flex gap-8 lg:gap-36">
+          <div>
+            <p className="index uppercase text-gray-400">About</p>
+            <ul className="mt-4 flex flex-col gap-4">
+              {aboutItems.map((item) => (
+                <li key={item.title}>
+                  <Link href={item.href}>{item.title}</Link>
+                </li>
+              ))}
+              <a
+                target="_blank"
+                className={linkStyles}
+                href="https://www.berlin.de/ba-friedrichshain-kreuzberg/aktuelles/pressemitteilungen/"
+                rel="noreferrer"
+              >
+                Presse
+              </a>
+            </ul>
+          </div>
+          <div>
+            <p className="index uppercase text-gray-400">Weitere Informationen</p>
+            <ul className="mt-4 flex flex-col gap-4">
+              {informationItems.map((item) => (
+                <li key={item.title}>
+                  <Link href={item.href}>{item.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div>
-          <p className="index uppercase text-gray-400">Weitere Informationen</p>
-          <ul className="mt-4 flex flex-col gap-4">
-            {informationItems.map((item) => (
-              <li key={item.title}>
-                <Link href={item.href}>{item.title}</Link>
-              </li>
-            ))}
-          </ul>
+        <div className="mt-8 border-t border-gray-600 pt-8">
+          <Link href="https://twitter.com/BA_Xhain">
+            <TwitterIcon width={20} />
+          </Link>
+          <p className="mt-8 text-gray-400">© 2023 Bezirksamt Friedrichshain-Kreuzberg</p>
         </div>
-      </div>
-      <div className="mt-8 border-t border-gray-600 pt-8">
-        <Link href="https://twitter.com/BA_Xhain">
-          <TwitterIcon width={20} />
-        </Link>
-        <p className="mt-8 text-gray-400">© 2023 Bezirksamt Friedrichshain-Kreuzberg</p>
-      </div>
+      </Container>
     </div>
   )
 }
