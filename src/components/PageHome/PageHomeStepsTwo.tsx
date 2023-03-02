1a5a466ae9071bc28da16c7dae561b26f18c9dc1
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-02 11:41:20 +0100

    fix list styles

--- src/components/PageHome/PageHomeStepsTwo.tsx
+++ src/components/PageHome/PageHomeStepsTwo.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
-import { H3, Prose } from '../typography'
+import { H3 } from '../typography'
 
 export const PageHomeStepsTwo: React.FC = () => {
   return (
     <section>
       <H3>Schritt 2</H3>
-      <Prose className="!prose-ul:list-disc !prose-ul:pl-4">
-        <ul>
-          <li>
-            Entwicklung eines Freiflächenkonzepts im gesamten Graefekiez, ausgehend von Ergebnissen
-            der Beteiligung im Kerngebiet und den Ergebnissen der wissenschaftlichen
-            Begleitforschung.
-          </li>
-          <li>
-            Das Freiflächenkonzept wird als Ergebnis des Projekts den politischen Gremien der BVV
-            zur Entscheidung vorgelegt.
-          </li>
-        </ul>
-      </Prose>
+      <ul className="list-disc pl-4">
+        <li>
+          Entwicklung eines Freiflächenkonzepts im gesamten Graefekiez, ausgehend von Ergebnissen
+          der Beteiligung im Kerngebiet und den Ergebnissen der wissenschaftlichen Begleitforschung.
+        </li>
+
+        <li>
+          Das Freiflächenkonzept wird als Ergebnis des Projekts den politischen Gremien der BVV zur
+          Entscheidung vorgelegt.
+        </li>
+      </ul>
     </section>
   )
 }
