9782982c382e8020b57d3264e3a121ec3584733b
Sven König <sven@fixmycity.de>
2023-07-12 18:14:19 +0200

    fix sitemap

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -77,15 +77,7 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-sitemap',
       options: {
-        excludes: [
-          '/blog/post1/',
-          '/blog/post2/',
-          '/blog/post3/',
-          '/blog/post4/',
-          '/blog/post5/',
-          '/blog/post6/',
-          '/blog/post7/',
-        ],
+        excludes: ['/blog/*/'],
       },
     },
   ],
