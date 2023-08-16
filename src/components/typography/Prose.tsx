2005bb60d3e141dfa08e9f75e9377e9fc3ad1d6f
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-08-16 10:04:00 +0200

    update list style for posts

--- src/components/typography/Prose.tsx
+++ src/components/typography/Prose.tsx
@@ -10,10 +10,10 @@ export const Prose: React.FC<Props> = ({ className, children }) => {
   return (
     <div
       className={clsx(
-        className,
         'max-w-full',
         'prose prose-p:leading-6 prose-p:text-gray-900 prose-ul:list-none prose-ul:pl-0 prose-ul:text-gray-900 prose-li:m-0 prose-li:pl-0 prose-li:text-gray-900',
         'prose-code:before:content-none prose-code:after:content-none',
+        className
       )}
     >
       {children}
